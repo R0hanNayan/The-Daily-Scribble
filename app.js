6fba170aa0f51f6f8fac253c9457de73b20e8cf6
Rohan Nayan <rohannayan405@gmail.com>
2023-03-27 21:19:19 +0530

    Added DataBase for blog Posts

--- app.js
+++ app.js
@@ -3,25 +3,34 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require('lodash');
+const { default: mongoose } = require("mongoose");
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 const app = express();
+const connection = mongoose.createConnection("mongodb+srv://rohannayan405:rohannayan405@cluster0.mlrfqzu.mongodb.net/blogDB")
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+//DataBase
+const postSchema = new mongoose.Schema({
+  title: String,
+  content: String,
+  postImg: String
+})
 
-const posts = [];
+const Post = connection.model("Post", postSchema);
+
 const appID = "VVNdfqTZCwEWhLK3ndRLr0lYlUAC06lzsnXiS41Xaqs";
 
 //Home Page
 app.get("/", async function(req,res){
   const quote = await getQuote();
-  
-  res.render("home", {startingContent: homeStartingContent, newPosts: posts, Quote:quote[0], author:quote[1]});
-
+  Post.find({}).then(posts=>{
+    res.render("home", {startingContent: homeStartingContent, newPosts: posts, Quote:quote[0], author:quote[1]});
+  })
 });
 
 //About Page
@@ -42,14 +51,14 @@ app.get('/compose', function(req, res){
 
 app.post('/compose', async function(req,res){
   const img = await getImg(req.body.newContentTitle);
-  const post = {
-    postTitle: req.body.newContentTitle,
-    postBody: req.body.newContent,
+  const post = new Post({
+    title: req.body.newContentTitle,
+    content: req.body.newContent,
     postImg: img
-  };
-  posts.push(post);
-  console.log(posts);
-  res.redirect('/');
+  });
+  post.save().then(function(){
+    res.redirect('/');
+  });
 });
 
 //API for post's images
@@ -72,15 +81,11 @@ async function getQuote(){
 }
 
 //Posts Page
-app.get('/posts/:topic', function(req, res){
-  const reqTitle =_.kebabCase(req.params.topic);
-
-  posts.forEach(function(post){
-    const storedTitle = _.kebabCase(post.postTitle);
-    if(storedTitle === reqTitle){
-      res.render("post", {postTitle: post.postTitle, postBody: post.postBody});
-    }
-  });
+app.get('/posts/:postID', function(req, res){
+  const reqPostID = req.params.postID;
+  Post.findOne({_id: reqPostID}).then(post=>{
+    res.render("post", {postTitle: post.title, postBody: post.content});
+  })
 });
 
 
