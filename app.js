f1d6e614b1749908739b3370907c0db673e223a9
Rohan Nayan <rohannayan405@gmail.com>
2023-04-24 13:40:21 +0530

    Added footer images

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 //jshint esversion:6
 const express = require("express");
 const bodyParser = require("body-parser");
-const ejs = require("ejs");
-const _ = require('lodash');
 const { default: mongoose } = require("mongoose");
 const homeStartingContent = "Welcome to The Daily Scribble, where words come alive! We're a blog that's dedicated to bringing you daily doses of inspiration, insights, and ideas. Our mission is to provide you with a space to explore your creativity, cultivate your writing skills, and share your thoughts with the world. Whether you're a seasoned writer or a beginner, we believe that everyone has a story to tell and a unique perspective to share. So, join us on this journey of self-discovery and expression, and let your imagination run wild!";
 const aboutContent = "Hello there! My name is Rohan Nayan and I am a Computer Science student at VIT Vellore. I am an avid gamer, music lover, and coding enthusiast. I also enjoy playing football in my free time. I created The Daily Scribble as a platform to share my thoughts and ideas with others. Through this blog, I hope to provide readers with a space to explore their creativity, learn something new every day, and connect with like-minded individuals. I am passionate about technology and its applications in our daily lives, and I believe that writing can be a powerful tool to inspire, inform, and transform. Thank you for visiting The Daily Scribble, and I hope you enjoy your time here!";
